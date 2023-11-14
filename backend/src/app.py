d725f923a9fae27b382463184d6eb17469bab2ea
gkalomalos <georgios.kalomalos@sword-group.com>
2023-11-14 16:21:43 +0200

    chore/Restructure backend directory and remove unnecessary files

---
+++ backend/src/app.py
@@ -0,0 +1,39 @@
+import sys
+import json
+
+from run_scenario import run_scenario
+
+
+def process_message(message):
+    script_name = message["scriptName"]
+    data = message["data"]
+
+    if script_name == "run_scenario.py":
+        result = run_scenario(data)
+        response = {"success": True, "result": result}
+    else:
+        response = {"success": False, "error": f"Unknown script: {script_name}"}
+
+    return response
+
+
+def main():
+    # Send the 'ready' event to the Node.js process
+    ready_event = {"type": "event", "name": "ready"}
+    print(json.dumps(ready_event))
+    sys.stdout.flush()
+
+    while True:
+        raw_message = sys.stdin.readline().strip()
+
+        if raw_message:
+            message = json.loads(raw_message)
+            response = process_message(message)
+            # Send the response back through stdout as a JSON string
+            sys.stdout.write(json.dumps(response) + "\n")
+            sys.stdout.flush()
+
+
+# Add this condition to ensure the infinite loop is only executed when app.py is run as the main script
+if __name__ == "__main__":
+    main()
