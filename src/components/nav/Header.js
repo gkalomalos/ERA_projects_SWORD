a52c649edaf4233663744a64cb391ecc7e698946
gkalomalos <georgios.kalomalos@sword-group.com>
2023-11-26 21:05:41 +0200

    feat/Update the Header module to include Language selector

--- src/components/nav/Header.js
+++ src/components/nav/Header.js
@@ -1,53 +1,67 @@
 import * as React from "react";
+
 import AppBar from "@mui/material/AppBar";
+import Grid from "@mui/material/Grid";
+import MenuItem from "@mui/material/MenuItem";
+import Select from "@mui/material/Select";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
-import Grid from "@mui/material/Grid";
 
-import css from "./Header.module.css";
 import climada_logo from "../../assets/climada_logo.png";
+import css from "./Header.module.css";
 
 const Header = () => {
+  const [language, setLanguage] = React.useState("EN");
+
+  const handleLanguageChange = (event) => {
+    setLanguage(event.target.value);
+  };
+
   return (
     <AppBar position="static" sx={{ bgcolor: "#2A4D69" }}>
-      <Grid container spacing={1}>
-        <Grid item xs={5}>
-          <img
-            src={climada_logo}
-            alt="climada_logo"
-            className={css.logo_climada}
-          />
-        </Grid>
-        <Grid item xs={6}>
-          <Toolbar disableGutters>
-            <Typography
-              variant="h3"
-              fontFamily={[
-                '"FFKievitStdBook"',
-                "Calibri",
-                "-apple-system",
-                "BlinkMacSystemFont",
-                '"Segoe UI"',
-                "Roboto",
-                '"Helvetica Neue"',
-                "Arial",
-                '"Noto Sans"',
-                "sans-serif",
-                '"Apple Color Emoji"',
-                '"Segoe UI Emoji"',
-                '"Segoe UI Symbol"',
-                '"Noto Color Emoji"',
-              ].join(",")}
-              noWrap
-              component="div"
-              sx={{ m: 1.5, display: { xs: "none", md: "flex" } }}
-            >
-              CLIMADA UNU
-            </Typography>
-          </Toolbar>
-        </Grid>
-      </Grid>
+      <Toolbar
+        disableGutters
+        sx={{ width: "100%", display: "flex", justifyContent: "space-between" }}
+      >
+        <img src={climada_logo} alt="climada_logo" className={css.logo_climada} />
+        <Typography
+          variant="h3"
+          fontFamily={[
+            '"FFKievitStdBook"',
+            "Calibri",
+            "-apple-system",
+            "BlinkMacSystemFont",
+            '"Segoe UI"',
+            "Roboto",
+            '"Helvetica Neue"',
+            "Arial",
+            '"Noto Sans"',
+            "sans-serif",
+            '"Apple Color Emoji"',
+            '"Segoe UI Emoji"',
+            '"Segoe UI Symbol"',
+            '"Noto Color Emoji"',
+          ].join(",")}
+          noWrap
+          component="div"
+          sx={{ m: 1.5, display: { xs: "none", md: "flex" } }}
+        >
+          CLIMADA UNU
+        </Typography>
+        <Select
+          value={language}
+          onChange={handleLanguageChange}
+          displayEmpty
+          inputProps={{ "aria-label": "Without label" }}
+          sx={{ m: 1, color: "white", width: 120 }}
+        >
+          <MenuItem value="EN">English</MenuItem>
+          <MenuItem value="AR">Arabic</MenuItem>
+          <MenuItem value="TH">Thai</MenuItem>
+        </Select>
+      </Toolbar>
     </AppBar>
   );
 };
+
 export default Header;
