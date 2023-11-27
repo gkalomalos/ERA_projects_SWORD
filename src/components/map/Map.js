1b3d2c6f2d47d1eb2a07347590f30cbbf6b956fd
gkalomalos <georgios.kalomalos@sword-group.com>
2023-11-27 10:53:07 +0200

    fix/Refactor layer controls to set 0-1-2 admin layers

--- src/components/map/Map.js
+++ src/components/map/Map.js
@@ -8,19 +8,51 @@ import "leaflet/dist/leaflet.css";
 
 const Map = ({ activeMap }) => {
   const [mapInfo, setMapInfo] = useState({ geoJson: null, colorScale: null });
+  const [activeLayer, setActiveLayer] = useState("0"); // State to track the active layer
+
+  // Mapping of layer names to specific numbers or strings
+  const layerMapping = {
+    "Admin level 0": "0",
+    "Admin level 1": "1",
+    "Admin level 2": "2",
+  };
 
   const LayerControls = ({ layers }) => {
     const map = useMap();
 
     useEffect(() => {
       const control = L.control.layers(null, layers).addTo(map);
+
+      // Modify DOM to change checkboxes to radio buttons and attach event listeners
+      const layerControlElement = control.getContainer();
+      const inputs = layerControlElement.getElementsByTagName("input");
+      for (let i = 0; i < inputs.length; i++) {
+        inputs[i].type = "radio";
+        inputs[i].name = "leaflet-base-layers"; // Ensure all radios have the same name
+        inputs[i].onchange = (e) => {
+          const layerName = e.target.value;
+          const mappedValue = layerMapping[layerName]; // Get the mapped value
+          setActiveLayer(mappedValue); // Update active layer state with the mapped value
+        };
+      }
+
+      // Set default layer (optional)
+      if (inputs.length > 0) {
+        inputs[0].checked = true;
+        setActiveLayer(layerMapping[Object.keys(layers)[0]]); // Set initial active layer using mapping
+      }
+
       return () => {
         control.remove();
       };
-    }, [map, layers]);
+    }, [map, layers, layerMapping]);
 
     return null;
   };
+
+  useEffect(() => {
+    console.log("Active Layer:", activeLayer); // Log the active layer whenever it changes
+  }, [activeLayer]);
 
   const layers = {
     "Admin level 0": L.layerGroup(),
@@ -31,7 +63,10 @@ const Map = ({ activeMap }) => {
   useEffect(() => {
     const fetchGeoJson = async () => {
       try {
-        const response = await fetch(`./${activeMap}_geodata_layer_1.json`);
+        // TODO: Change to dynamic
+        const response = await fetch(
+          `C:\\Users\\gkalomalos\\Projects\\unu\\climada-unu\\src\\components\\map\\${activeMap}_geodata_layer_${activeLayer}.json`
+        );
         if (!response.ok) {
           // Check if the response is not ok (404 or other HTTP errors)
           throw new Error(`HTTP error! status: ${response.status}`); // Throw an error to go to the catch block
@@ -66,7 +101,7 @@ const Map = ({ activeMap }) => {
 
   const onEachFeature = (feature, layer) => {
     if (feature.properties) {
-      layer.bindPopup(`Name: ${feature.properties.NAME_1}<br>Value: ${feature.properties.value}`);
+      layer.bindPopup(`Name: ${feature.properties.GID_1}<br>Value: ${feature.properties.value}`);
     }
   };
 
