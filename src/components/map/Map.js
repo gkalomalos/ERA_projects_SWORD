79d558c3902cce1980af367a23f56cd2666d3d64
gkalomalos <georgios.kalomalos@sword-group.com>
2023-11-27 15:27:57 +0200

    chore/Rollback layers functionality in Map component

--- src/components/map/Map.js
+++ src/components/map/Map.js
@@ -1,47 +1,22 @@
 import React, { useEffect, useState } from "react";
 
-import L from "leaflet";
-import { MapContainer, TileLayer, GeoJSON, useMap } from "react-leaflet";
+import { MapContainer, TileLayer, GeoJSON } from "react-leaflet";
 import { scaleSequential } from "d3-scale";
 import { interpolateRdYlGn } from "d3-scale-chromatic";
 import "leaflet/dist/leaflet.css";
 
 const Map = ({ activeMap }) => {
   const [mapInfo, setMapInfo] = useState({ geoJson: null, colorScale: null });
-  const [activeLayer, setActiveLayer] = useState("1"); // State to track the active layer
-
-  const LayerControls = ({ layers }) => {
-    const map = useMap();
-
-    useEffect(() => {
-      const control = L.control.layers(null, layers).addTo(map);
-
-      return () => {
-        control.remove();
-      };
-    }, [map, layers]);
-  };
-
-  useEffect(() => {
-    console.log("Active Layer:", activeLayer); // Log the active layer whenever it changes
-  }, [activeLayer]);
-
-  const layers = {
-    "Admin level 0": L.layerGroup(),
-    "Admin level 1": L.layerGroup(),
-    "Admin level 2": L.layerGroup(),
-  };
+  const [activeLayer, setActiveLayer] = useState(1);
 
   useEffect(() => {
     const fetchGeoJson = async () => {
       try {
-        // TODO: Change to dynamic
         const response = await fetch(
           `C:\\Users\\gkalomalos\\Projects\\unu\\climada-unu\\src\\components\\map\\${activeMap}_geodata_layer_${activeLayer}.json`
         );
         if (!response.ok) {
-          // Check if the response is not ok (404 or other HTTP errors)
-          throw new Error(`HTTP error! status: ${response.status}`); // Throw an error to go to the catch block
+          throw new Error(`HTTP error! status: ${response.status}`);
         }
         const data = await response.json();
         const values = data.features.map((f) => f.properties.value);
@@ -51,10 +26,11 @@ const Map = ({ activeMap }) => {
 
         setMapInfo({ geoJson: data, colorScale: scale });
       } catch (error) {
-        console.error("Error fetching GeoJSON data for " + activeMap, error);
-        setMapInfo({ geoJson: null, colorScale: null }); // Set to default state for empty map
+        console.error("Error fetching GeoJSON data:", error);
+        setMapInfo({ geoJson: null, colorScale: null });
       }
     };
+
     if (activeMap) {
       fetchGeoJson();
     }
@@ -78,13 +54,13 @@ const Map = ({ activeMap }) => {
   };
 
   return (
-    <MapContainer center={[27.5, 31.0]} zoom={7} style={{ height: "100%", width: "100%" }}>
-      <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+    <MapContainer center={[30.0, 31.0]} zoom={7} style={{ height: "100%", width: "100%" }}>
+      <TileLayer
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+      />
       {mapInfo.geoJson && mapInfo.colorScale && (
-        <>
-          <LayerControls layers={layers} />
-          <GeoJSON data={mapInfo.geoJson} style={style} onEachFeature={onEachFeature} />
-        </>
+        <GeoJSON data={mapInfo.geoJson} style={style} onEachFeature={onEachFeature} />
       )}
     </MapContainer>
   );
