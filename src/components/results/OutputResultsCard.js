280f4e008adc76ceb0a69b3295b66b145dc279a5
gkalomalos <georgios.kalomalos@sword-group.com>
2024-05-28 11:47:48 +0300

    feat/Add new OutputResultsCard component with the export buttons

--- src/components/results/OutputResultsCard.js
+++ src/components/results/OutputResultsCard.js
@@ -1,12 +1,60 @@
 import React from "react";
+import { useTranslation } from "react-i18next";
 
-// import PageUnderConstructionView from "../misc/PageUnderConstructionView";
+import { Box, Button } from "@mui/material";
 
 const OutputResultsCard = () => {
+  const { t } = useTranslation();
+  const handleButtonClick = (type) => {
+    console.log(type);
+  };
+
   return (
     <>
-      test
-      {/* <PageUnderConstructionView />; */}
+      <Box sx={{ display: "flex", flexDirection: "column", height: "100%" }}>
+        <Box sx={{ display: "flex", justifyContent: "space-around", marginBottom: 2 }}>
+          <Box sx={{ display: "flex", flexDirection: "column", marginBottom: 2 }}>
+            {["pdf", "excel", "word", "gis"].map((type) => (
+              <Button
+                key={type}
+                variant="contained"
+                sx={{
+                  marginBottom: 2,
+                  bgcolor: "#FFCCCC",
+                  transition: "transform 0.1s ease-in-out",
+                  "&:active": {
+                    transform: "scale(0.96)",
+                  },
+                  "&:hover": { bgcolor: "#F79191" },
+                }}
+                onClick={() => handleButtonClick(type)}
+              >
+                {t(`results_export_button_${type}`)}
+              </Button>
+            ))}
+          </Box>
+          <Box sx={{ display: "flex", flexDirection: "column", marginBottom: 2 }}>
+            {["gis", "ppt", "other"].map((type) => (
+              <Button
+                key={type}
+                variant="contained"
+                sx={{
+                  marginBottom: 2,
+                  bgcolor: "#FFCCCC",
+                  transition: "transform 0.1s ease-in-out",
+                  "&:active": {
+                    transform: "scale(0.96)",
+                  },
+                  "&:hover": { bgcolor: "#F79191" },
+                }}
+                onClick={() => handleButtonClick(type)}
+              >
+                {t(`results_export_button_${type}`)}
+              </Button>
+            ))}
+          </Box>
+        </Box>
+      </Box>
     </>
   );
 };
