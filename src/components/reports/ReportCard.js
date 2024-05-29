6e6e8085f52fd03aee816aa08cd1b8f0ce12dfbc
gkalomalos <georgios.kalomalos@sword-group.com>
2024-05-29 12:35:10 +0300

    feat/Add dynamic report data and dummy images

--- src/components/reports/ReportCard.js
+++ src/components/reports/ReportCard.js
@@ -1,14 +1,14 @@
 /* eslint-disable no-unused-vars */
 import React, { useState } from "react";
+import PropTypes from "prop-types";
 import { useTranslation } from "react-i18next";
-import image from "./risks_waterfall_plot.png";
 
 import { Box, Typography, IconButton, List, ListItem, ListItemText } from "@mui/material";
 import { Delete, ArrowUpward, ArrowDownward } from "@mui/icons-material";
 
 import AlertMessage from "../alerts/AlertMessage";
 
-const ReportsCard = () => {
+const ReportsCard = ({ data, image, title, type }) => {
   const { t } = useTranslation();
 
   const [message, setMessage] = useState("");
@@ -20,6 +20,9 @@ const ReportsCard = () => {
   };
 
   const handleDeleteButtonClick = () => {
+    setMessage("Report deleted successfully.");
+    setSeverity("success");
+    setShowMessage(true);
     console.log("delete");
   };
 
@@ -32,75 +35,95 @@ const ReportsCard = () => {
   };
 
   return (
-    <Box
-      sx={{
-        alignItems: "flex-start",
-        border: "1px solid #ccc",
-        borderRadius: "16px",
-        display: "flex",
-        flexDirection: "row",
-        marginBottom: 2,
-        width: "100%",
-      }}
-    >
-      <Box sx={{ flex: 1, maxWidth: "10%" }}>
-        <List dense disablePadding>
-          <ListItem>
-            <ListItemText primary="Title:" />
-          </ListItem>
-          <ListItem>
-            <ListItemText secondary="Type:" />
-          </ListItem>
-          <ListItem>
-            <ListItemText secondary="Data:" />
-          </ListItem>
-        </List>
-      </Box>
+    <>
+      <Box
+        sx={{
+          alignItems: "flex-start",
+          border: "1px solid #ccc",
+          borderRadius: "16px",
+          display: "flex",
+          flexDirection: "row",
+          marginBottom: 2,
+          width: "100%",
+        }}
+      >
+        <Box sx={{ flex: 1, maxWidth: "10%" }}>
+          <List dense disablePadding>
+            <ListItem>
+              <ListItemText primary="Title:" />
+            </ListItem>
+            <ListItem>
+              <ListItemText secondary="Type:" />
+            </ListItem>
+            <ListItem>
+              <ListItemText secondary="Data:" />
+            </ListItem>
+          </List>
+        </Box>
 
-      <Box sx={{ flex: 2, maxWidth: "55%" }}>
-        <List dense disablePadding>
-          <ListItem>
-            <ListItemText primary="Flood Expansion – return period 1 in 100 years" />
-          </ListItem>
-          <ListItem>
-            <ListItemText secondary="Economic – Risk – Hazard – Map" />
-          </ListItem>
-          <ListItem>
-            <ListItemText secondary="Thailand/2050/SSP2-4.5/Flood/Markets/GDP2/..." />
-          </ListItem>
-        </List>
-      </Box>
+        <Box sx={{ flex: 2, maxWidth: "55%" }}>
+          <List dense disablePadding>
+            <ListItem>
+              <ListItemText primary={title} />
+            </ListItem>
+            <ListItem>
+              <ListItemText secondary={type} />
+            </ListItem>
+            <ListItem>
+              <ListItemText secondary={data} />
+            </ListItem>
+          </List>
+        </Box>
 
-      <Box sx={{ flex: 3, maxWidth: "25%" }}>
-        <Box
-          component="img"
-          sx={{
-            width: "128px",
-            height: "100%",
-          }}
-          alt="report_image"
-          src={image}
-        />
-      </Box>
+        <Box sx={{ flex: 3, maxWidth: "25%" }}>
+          <Box
+            component="img"
+            sx={{
+              width: "128px",
+              height: "100%",
+            }}
+            alt="report_image"
+            src={image}
+          />
+        </Box>
 
-      <Box>
-        <Box sx={{ display: "flex", flexDirection: "column", alignItems: "start", width: "100%" }}>
-          <IconButton aria-label="delete" onClick={handleDeleteButtonClick} size="small" >
-            <Delete fontSize="small" />
-            <Typography>Remove</Typography>
-          </IconButton>
-          <IconButton aria-label="move_up" onClick={handleUpButtonClick} size="small">
-            <ArrowUpward fontSize="small" />
-            <Typography>Move Up</Typography>
-          </IconButton>
-          <IconButton aria-label="move_down" onClick={handleDownButtonClick} size="small">
-            <ArrowDownward fontSize="small" />
-            <Typography>Move Down</Typography>
-          </IconButton> 
+        <Box>
+          <Box
+            sx={{ display: "flex", flexDirection: "column", alignItems: "start", width: "100%" }}
+          >
+            <IconButton aria-label="delete" onClick={handleDeleteButtonClick} size="small">
+              <Delete fontSize="small" />
+              <Typography>Remove</Typography>
+            </IconButton>
+            <IconButton aria-label="move_up" onClick={handleUpButtonClick} size="small">
+              <ArrowUpward fontSize="small" />
+              <Typography>Move Up</Typography>
+            </IconButton>
+            <IconButton aria-label="move_down" onClick={handleDownButtonClick} size="small">
+              <ArrowDownward fontSize="small" />
+              <Typography>Move Down</Typography>
+            </IconButton>
+          </Box>
         </Box>
       </Box>
-    </Box>
+      {/* Alert message section */}
+      {message && showMessage && (
+        <AlertMessage
+          handleCloseMessage={handleCloseMessage}
+          message={message}
+          severity={severity}
+          showMessage={showMessage}
+        />
+      )}
+    </>
   );
+};
+
+ReportsCard.propTypes = {
+  data: PropTypes.string.isRequired,
+  image: PropTypes.any.isRequired,
+  title: PropTypes.string.isRequired,
+  type: PropTypes.string.isRequired,
 };
 
 export default ReportsCard;
--- src/components/reports/ReportsView.js
+++ src/components/reports/ReportsView.js
@@ -1,13 +1,32 @@
 import React from "react";
 
+import image1 from "./cost_benefit_plot.png";
+import image2 from "./risks_waterfall_plot_1.png";
+import image3 from "./risks_waterfall_plot_2.png";
+
 import ReportCard from "./ReportCard";
 
 const ReportsView = () => {
   return (
     <>
-      <ReportCard />
-      <ReportCard />
-      <ReportCard />
+      <ReportCard
+        data="Thailand/2050/SSP2-4.5/Flood/Markets/GDP2/..."
+        image={image2}
+        title="Flood Expansion – return period 1 in 100 years"
+        type="Economic – Risk – Hazard – Map"
+      />
+      <ReportCard
+        data="Thailand/2050/SSP2-4.5/Flood/Markets/GDP2/…"
+        image={image3}
+        title="Exposure of Markets"
+        type="Economic – Risk – Exposure – Map "
+      />
+      <ReportCard
+        data="Thailand/2050/SSP2-4.5/Flood/Markets/GDP2/..."
+        image={image1}
+        title="Cost-Benefit Analysis of Adaptation Measures"
+        type="Economic – Adaptation – Cost-Benefit – Chart "
+      />
     </>
   );
 };
