ba6f02a8637e0d21e86939f0e8e19a0c241c8185
gkalomalos <georgios.kalomalos@sword-group.com>
2024-07-16 17:51:01 +0300

    fix/Adjust the legend to correctly present the negative values

--- src/utils/colorScales.js
+++ src/utils/colorScales.js
@@ -6,7 +6,7 @@ const getColorScale = (hazard) => {
     case "flood":
       return schemeBlues[k].slice(-5); // Get last 5 colors
     case "drought":
-      return [...schemeYlOrBr[k]].slice(-5);
+      return [...schemeYlOrBr[k]].slice(-5).reverse();
     case "heatwaves":
       return schemeReds[k].slice(-5);
     default:
@@ -16,15 +16,28 @@ const getColorScale = (hazard) => {
 
 export const getScale = (hazard, percentileValues) => {
   const colors = getColorScale(hazard);
+  // Ensure the percentileValues are sorted (important if values can be in any order)
+  percentileValues.sort((a, b) => a - b);
 
-  // Create boundary thresholds based on percentile values
   return (value) => {
-    for (let i = 0; i < percentileValues.length - 1; i++) {
-      if (value >= percentileValues[i] && value < percentileValues[i + 1]) {
-        return colors[i];
+    // Determine if values are in ascending or descending order
+    const isAscending = percentileValues[0] < percentileValues[percentileValues.length - 1];
+
+    if (isAscending) {
+      for (let i = 0; i < percentileValues.length - 1; i++) {
+        if (value >= percentileValues[i] && value < percentileValues[i + 1]) {
+          return colors[i];
+        }
+      }
+    } else {
+      // Handle descending order values
+      for (let i = percentileValues.length - 1; i > 0; i--) {
+        if (value <= percentileValues[i] && value > percentileValues[i - 1]) {
+          return colors[percentileValues.length - i - 1];
+        }
       }
     }
-    // Handle the case where value is equal to or greater than the last threshold
-    return colors[colors.length - 1];
+    // Handle the edge case for the last segment
+    return colors[isAscending ? colors.length - 1 : 0];
   };
 };
