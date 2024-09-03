9f5c6f8a2c5577674db3d50005a02756dde93ea7
gkalomalos <georgios.kalomalos@sword-group.com>
2024-09-03 14:28:46 +0300

    chore/Remove report details section from output tab

--- src/components/results/OutputResultsCard.js
+++ src/components/results/OutputResultsCard.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { useTranslation } from "react-i18next";
 
-import { Box, Button, Typography } from "@mui/material";
+import { Box, Button } from "@mui/material";
 
 import APIService from "../../APIService";
 import useStore from "../../store";
@@ -95,35 +95,6 @@ const OutputResultsCard = () => {
               </Button>
             ))}
           </Box>
-        </Box>
-        {/* Result Details section */}
-        <Box
-          sx={{
-            bgcolor: "#FFCCCC",
-            padding: 2,
-            borderRadius: "4px",
-          }}
-        >
-          <Typography
-            variant="h6"
-            sx={{
-              borderBottom: "1px solid #6F6F6F",
-              paddingBottom: 1,
-              color: "#6F6F6F",
-              textAlign: "center",
-            }}
-          >
-            {t("results_report_details")}
-          </Typography>
-          {/* Content here will grow to fill available space */}
-          <Typography variant="body1" sx={{ marginTop: 2, flexGrow: 1, color: "#6F6F6F" }}>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
-            incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
-            exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure
-            dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-            Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt
-            mollit anim id est laborum.
-          </Typography>
         </Box>
       </Box>
     </>
